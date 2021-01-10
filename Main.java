fb7127cd5bf5636aebb16f7ceeae7a1abf1a8db1
andyreigo <andresgomezreinoso@gmail.com>
2021-01-10 14:27:12 -0400

    count nodes in a bst exercise

--- Main.java
+++ Main.java
@@ -1,6 +1,14 @@
 public class Main {
     
     public static void main(String[] args){
-        
+        Node root = new Node(2);  
+    root.left = new Node(7);  
+    root.right = new Node(5);  
+    root.left.right = new Node(6);  
+    root.left.right.left = new Node(1);  
+    root.left.right.right = new Node(11);  
+    root.right.right = new Node(9);  
+    root.right.right.left = new Node(4);
+        System.out.println(root.getNodesCountIterative(root));
     }
 }
--- Node.java
+++ Node.java
@@ -1,8 +1,46 @@
+import java.util.LinkedList;
+import java.util.Queue; 
+
 public class Node{
     int data;
-    Node next;
-    Node prev;
+    Node left;
+    Node right;
+   
+    // If given the Head Node create a function that gives you the count of Nodes in the linked list
 
-    
+    public Node(int data){
+        this.data=data;
+        this.left=null;
+        this.right=null;
+    }
+    public int getNodesCount(Node node){
+       // count the head node
+       // if there are any nodes on the left and right check and add them to the total
+        if(node==null)
+        return 0;
+        int result=0;
+        result++;
+        result+=(getNodesCount(node.left)+getNodesCount(node.right));
+        // add 1 to the count
+         return result;     
+    }
 
+    public int getNodesCountIterative(Node node){
+        if(node==null)
+        return 0;
+        int result=0;
+        Queue<Node> queue=new LinkedList<>();
+        queue.add(node);
+        while(!queue.isEmpty()){
+            Node temp =queue.poll();
+            result++;
+            if(temp.left!=null){
+                queue.add(temp.left);
+            }
+            if(temp.right!=null){
+                queue.add(temp.right);
+            }
+        }
+        return result;
+    }
 }
